8a332f83d88d6120cf11ec9ca832247bc9577bde
Shahadat5932 <shahadathossain5932@gmail.com>
2021-10-03 01:38:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './compnoents/Home/Home';
+import Friends from './compnoents/Friends/Friends';
+import About from './compnoents/About/About';
+import NotFound from'./compnoents/NotFound/NotFound';
+import { BrowserRouter, Switch, Route,} from 'react-router-dom';
+import Header from './compnoents/Header/Header';
+import FriendDetail from './compnoents/FriendDetail/FriendDetail';
+import Culture from './compnoents/Culture/Culture';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <BrowserRouter>
+     <Header></Header>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/about">
+           <About></About>
+          </Route>
+            <Route path="/friend/:friendId">
+              <FriendDetail></FriendDetail>
+            </Route>
+            <Route path="/about/culture">
+            <Culture></Culture>
+            </Route>
+          <Route path="/friends">
+            <Friends></Friends>
+          </Route>
+          <Route exact="/*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+     </BrowserRouter>
     </div>
   );
 }
